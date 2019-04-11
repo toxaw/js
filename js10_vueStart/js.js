bba2513a4ce1573bd855827ab9e689f4f063c344
toxaw <antonser2@gmail.com>
2019-04-11 22:49:23 +0400

    rename ext

---
+++ js10_vueStart/js.js
@@ -0,0 +1,195 @@
+let searchBlock = 
+{
+	findEvent : (callback) =>
+	{
+		let element = document.getElementById('search-container');
+
+		let inputSearch, inputCheckbox;
+
+		Array.from(element.getElementsByTagName('input')).forEach
+		(
+			(input) =>
+			{
+		  		if(input.type=='text')
+		  		{
+		  			inputSearch = input;
+		  		}
+		  		else if(input.type=='checkbox')
+		  		{
+		  			inputCheckbox = input;
+		  		}
+			}
+		);
+
+		let iStatic = element.getElementsByClassName('static')[0];
+
+		inputSearch.addEventListener('keydown', (e)=>
+			{
+				if(e.keyCode==13)
+				{
+					if(inputSearch.value)
+					{
+						callback(inputSearch.value);
+
+						element = document.getElementById('search-danger');
+
+						element.innerText = '';
+
+						inputCheckbox.checked = true;						
+					}
+				}
+			}
+		);
+
+		iStatic.addEventListener('mousedown', (e)=>
+			{
+				if(inputSearch.value)
+				{
+					callback(inputSearch.value);
+
+					element = document.getElementById('search-danger');
+
+					element.innerText= '';
+
+					inputCheckbox.checked = true;					
+				}
+				else
+				{
+					inputCheckbox.checked = false;
+				}
+			}
+		);					
+	},
+	done : (danger = '') =>
+	{
+		let element = document.getElementById('search-container');
+
+		let inputCheckbox;
+
+		Array.from(element.getElementsByTagName('input')).forEach
+		(
+			(input) =>
+			{
+		  		if(input.type=='checkbox')
+		  		{
+		  			inputCheckbox = input;
+		  		}
+			}
+		);
+
+		inputCheckbox.checked = false;
+
+		element = document.getElementById('search-danger');
+
+		element.innerText = danger;		
+	}
+};
+
+let contentBlock = 
+{
+	setContent:(contentList) =>
+	{
+		let contentField = document.getElementsByTagName('main')[0];
+
+		contentList.forEach
+		(
+			function(element) 
+			{
+				imageElement = document.createElement('img');
+
+				imageElement.src = element.src;
+
+				headerElement = document.createElement('p');
+
+				headerElement.innerText = element.text;
+
+  				divElement = document.createElement('div');
+
+  				divElement.className = 'film-element';
+
+  				divElement.appendChild(headerElement);
+
+  				divElement.appendChild(imageElement);
+
+  				contentField.appendChild(divElement);
+			}
+		);
+	},
+	removeContent:() =>
+	{
+		let contentField = document.getElementsByTagName('main')[0];
+		
+		Array.from(contentField.getElementsByClassName('film-element')).forEach 
+		(
+			(element) =>
+			{
+				contentField.removeChild(element);				
+			}
+		);
+
+	}
+};
+
+let api = 
+{
+	query:async (query, callback) =>
+	{
+		let url = 'http://www.omdbapi.com/?s=' + encodeURI(query.toLowerCase()) + '&apikey=d5677312';
+		
+		try
+		{
+			let response = await fetch(url);
+		
+			let data = await response.json();
+
+			callback(data);
+		}
+		catch(e)
+		{
+			callback(null, 'Ошибка сервера');
+		}
+	}
+};
+
+searchBlock.findEvent
+(
+	(search) =>
+	{
+		api.query(search, (content, text = null) =>
+			{
+				contentBlock.removeContent();
+
+				if(content && content['Search'])
+				{
+					contentList = [];
+
+					Array.from(content['Search']).forEach
+					(
+						(element) =>
+						{
+							contentList.push
+							(
+								{
+									src: element.Poster!='N/A'?element.Poster:'image/not_found.jpg',
+									text: element.Title + ' ' + (element.Year!='N/A'?`(${element.Year} год)`:'')
+								}
+							);
+						}
+					);
+
+					contentBlock.setContent(contentList);
+						
+					searchBlock.done();
+				}
+				else if (!text && !content['Search']) 
+				{
+					searchBlock.done('Поиск не дал результатов');
+				}
+				else
+				{
+					searchBlock.done(text?text:'');
+				}
+			}
+		);
+	}
+);
